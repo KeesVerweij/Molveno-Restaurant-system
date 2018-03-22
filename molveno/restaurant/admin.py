8b8192446770c70699b57128b57bd8e805d3ae4e
KeesVerweij <kees.verweij@yacht.nl>
2018-03-22 14:23:11 +0100

    updated the view of the inventory table to show more details

--- molveno/restaurant/admin.py
+++ molveno/restaurant/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Inventory
 
-admin.site.register(Inventory)
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('description', 'brand', 'supplier', 'article_number',
+                    'unit', 'container_amount', 'price', 'unit_price',
+                    'minimum_quantity', 'current_stock', 'order_quantity')
+
+
+admin.site.register(Inventory, InventoryAdmin)
