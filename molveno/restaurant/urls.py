d149471b8dd955be150dfd5e2425711d6e89cf27
Ruben Cronie <ruben.cronie@yacht.nl>
2018-04-05 16:00:53 +0200

    custom admin view

--- molveno/restaurant/urls.py
+++ molveno/restaurant/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 app_name = 'restaurant'
 urlpatterns = [
-    path('inventory/',views.InventoryPageView.as_view(),name="inventory"),
+    path('restaurant/',views.IndexView.as_view(),name="index"),
+    path('inventory/',views.InventoryPageView.as_view(), name = "inventory"),
+
+    path('restaurant/Menu/', views.MenuChangeView.as_view(), name = "menu-change-view"),
+    path('restaurant/Menu/add/', views.MenuAddView.as_view(), name = "menu-add-view"),
+
+    path('restaurant/Supplier/', views.SupplierChangeView.as_view(), name = "supplier-change-view"),
+    path('restaurant/Supplier/add/', views.SupplierAddView.as_view(), name = "supplier-add-view"),
 ]
