cd415f6e648a3a8affe119dac175cc941ef7ed8e
KeesVerweij <kees.verweij@yacht.nl>
2018-04-05 19:46:06 +0200

    simple flow for ordering with menu item detail page including form for adding an amount of this item to your order, a view that saves the order to the database, a database model and an admin page for viewing and managing orders. missing: page for confirming orders before placing them (they're now placed immediately) as well as viewing the placed order.

--- molveno/restaurant/urls.py
+++ molveno/restaurant/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'restaurant'
 urlpatterns = [
-    path('inventory/',views.InventoryPageView.as_view(),name="inventory"),
+    path('inventory/', views.InventoryPageView.as_view(), name="inventory"),
+    path('menuitem/<int:pk>/', views.MenuItemView.as_view(), name="menu_item"),
+    path('addorder/<int:item_id>/', views.AddOrderView, name="addorder"),
 ]
