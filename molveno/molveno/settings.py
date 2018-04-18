45c96270275d31b26ef2592baad61dcbb434b00c
Mari4 <mflorekonda@gmail.com>
2018-04-18 17:10:59 +0200

    Created some drinks, added "orders" app

--- molveno/molveno/settings.py
+++ molveno/molveno/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant.apps.RestaurantConfig',
-    'reservations'
+    'reservations',
+    'orders',
 ]
 
 MIDDLEWARE = [
