59b1e7a5b3d2c42952b64384b136dd017c845d8b
Lars Nijboer <larsnijboer1991@live.nl>
2018-04-20 16:22:07 +0200

    finalised orders app

--- molveno/orders/urls.py
+++ molveno/orders/urls.py
@@ -5,4 +5,8 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order_history/',views.OrderHistoryView.as_view(), name = 'order_history'),
+    path('main', views.IndexView.as_view(), name = 'index'),
+    path('food_orders/', views.FoodOrdersList.as_view(), name = 'food_orders'),
+    path('drink_orders/', views.DrinkOrdersList.as_view(), name = 'drink_orders'),
+    path('orders/recipe/<int:pk>/', views.OrderRecipe.as_view(), name='recipe'),
 ]
